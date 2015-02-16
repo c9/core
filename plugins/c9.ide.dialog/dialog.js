6a4bcaa1f43bfe3112027235a92202c8515df432
Ruben Daniels <ruben@c9.io>
2015-02-16 20:22:06 +0000

    More API improvements

--- plugins/c9.ide.dialog/dialog.js
+++ plugins/c9.ide.dialog/dialog.js
@@ -74,10 +74,13 @@ define(function(require, module, exports) {
             var modal = options.modal;
             var zindex = options.zindex;
             var allowClose = options.allowClose;
-            var elements = options.elements;
+            var elements = options.elements || [];
             var resizable = options.resizable || false;
             var widths = options.widths || {};
             var count = 0;
+            
+            if (custom === undefined)
+                custom = !(body || heading);
             
             var dialog, buttons, titles;
             
@@ -148,9 +151,14 @@ define(function(require, module, exports) {
                 buttons = plugin.getElement("buttons");
                 
                 // Create dynamic UI elements
-                elements.forEach(function(item) {
-                    createItem(null, null, item);
-                });
+                if (elements.length) {
+                    elements.forEach(function(item) {
+                        createItem(null, null, item);
+                    });
+                }
+                else {
+                    buttons.parentNode.removeChild(buttons);
+                }
                 
                 emit.sticky("draw", {
                     aml: titles,
