52980d2c3ae559066364dbc90135cfdc310b10f3
Ruben Daniels <ruben@c9.io>
2015-04-24 19:50:11 +0000

    Enable new plugin

--- plugins/c9.ide.plugins/manager.js
+++ plugins/c9.ide.plugins/manager.js
@@ -102,7 +102,6 @@ define(function(require, exports, module) {
         // var emit = plugin.getEmitter();
 
         var HASSDK = c9.location.indexOf("sdk=0") === -1;
-        var ENABLED = c9.location.indexOf("sdk=1") > -1;
 
         var model, datagrid, filterbox;
         var btnUninstall, btnReport, btnReadme, btnCloud9, btnReload;
@@ -125,20 +124,18 @@ define(function(require, exports, module) {
             //     updateCommandsFromSettings();
             // }, plugin);
 
-            if (ENABLED) {
-                menus.addItemByPath("File/New Plugin", null, 210, plugin);
-                Object.keys(TEMPLATES).forEach(function(name){
-                    menus.addItemByPath("File/New Plugin/" + TEMPLATES[name], new ui.item({
-                        onclick: function(){
-                            createNewPlugin(name);
-                        }
-                    }), 210, plugin);
-                });
-                
-                ext.on("register", function(){
-                    setTimeout(reloadModel);
-                });
-            }
+            menus.addItemByPath("File/New Plugin", null, 210, plugin);
+            Object.keys(TEMPLATES).forEach(function(name){
+                menus.addItemByPath("File/New Plugin/" + TEMPLATES[name], new ui.item({
+                    onclick: function(){
+                        createNewPlugin(name);
+                    }
+                }), 210, plugin);
+            });
+            
+            ext.on("register", function(){
+                setTimeout(reloadModel);
+            });
         }
 
         var drawn;
