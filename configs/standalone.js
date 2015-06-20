2e62cce882fb6c115ca2da836816d3b01d3f7f03
Lennart kats <lennart@c9.io>
2015-06-20 04:18:08 +0000

    Fix standalone dependency on analytics

--- configs/standalone.js
+++ configs/standalone.js
@@ -174,6 +174,7 @@ module.exports = function(config, optimist) {
         "./c9.vfs.server/download",
         "./c9.vfs.server/filelist",
         "./c9.vfs.server/statics",
+        "./c9.analytics/mock_analytics",
         "./c9.metrics/mock_metrics",
         "./c9.ide.experiment/mock_experiment",
         {
