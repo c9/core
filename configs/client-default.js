bba93a3239adb084423a82a83b3fbbd41254f9fe
Lennart kats <lennart@c9.io>
2015-06-24 16:01:56 +0000

    Fix standalone

--- configs/client-default.js
+++ configs/client-default.js
@@ -302,7 +302,7 @@ module.exports = function(options) {
         {
             packagePath: "plugins/c9.ide.language/language",
             workspaceDir: workspaceDir,
-            staticPrefix: options.packed ? staticPrefix : options.baseUrl + staticPrefix,
+            staticPrefix: options.packed || !options.baseUrl ? staticPrefix : options.baseUrl + staticPrefix,
             workerPrefix: options.CORSWorkerPrefix // "/static/standalone/worker"
         },
         "plugins/c9.ide.language/keyhandler",
