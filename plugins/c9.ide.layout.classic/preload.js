bc0b8e02d7b166445f8fd2d2155ab609c61c9909
nightwing <amirjanyan@gmail.com>
2015-07-03 03:59:11 +0400

    do not add sourceURL in architect-build

--- plugins/c9.ide.layout.classic/preload.js
+++ plugins/c9.ide.layout.classic/preload.js
@@ -49,12 +49,14 @@ define(function(require, exports, module) {
                     callback(err, data);
                 });
             } else {
-                http.request(themePrefix + "/" + name + ".css", {
+                var url = themePrefix + "/" + name + ".css";
+                http.request(url, {
                     timeout: 2 * 60 * 1000
                 }, function(err, data) {
                     if (err)
                         return callback(err, data);
-                    
+                    // set sourceurl so that sourcemaps work when theme is inserted as a style tag
+                    data += "\n/*# sourceURL=" + url + " */";
                     themes[name] = data;
                     callback(err, data);
                 });
