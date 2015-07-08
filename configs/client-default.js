f531a9ce9b849b6f62ca588cfce34eefb4242d04
nightwing <amirjanyan@gmail.com>
2015-07-08 16:07:20 +0400

    fix language worker regression in standalone mode

--- configs/client-default.js
+++ configs/client-default.js
@@ -302,9 +302,9 @@ module.exports = function(options) {
         {
             packagePath: "plugins/c9.ide.language/language",
             workspaceDir: workspaceDir,
-            staticPrefix: options.packed
-                ? staticPrefix
-                : options.ideBaseUrl + "/uph" + staticPrefix,
+            staticPrefix: hosted && !options.packed
+                ? options.ideBaseUrl + "/uph" + staticPrefix
+                : staticPrefix,
             workerPrefix: options.CORSWorkerPrefix // "/static/standalone/worker"
         },
         "plugins/c9.ide.language/keyhandler",
