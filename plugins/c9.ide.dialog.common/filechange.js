cca43ff21b869a74d1bffe1bab1f54a76c5a82e5
Tim Robinson <tim@c9.io>
2015-09-11 08:51:01 +0000

    Options is undefined sometimes causing this to crash

--- plugins/c9.ide.dialog.common/filechange.js
+++ plugins/c9.ide.dialog.common/filechange.js
@@ -6,6 +6,7 @@ define(function(require, module, exports) {
     function main(options, imports, register) {
         var Dialog = imports.Dialog;
         var util = imports.util;
+        options = options || {};
         
         /***** Initialization *****/
         
