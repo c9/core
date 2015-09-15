0d7243ee6faf7ae0391be29c54a83c05ff432404
Ruben Daniels <ruben@c9.io>
2015-09-15 06:20:27 +0000

    layout tweak

--- plugins/c9.ide.preferences/experimental.js
+++ plugins/c9.ide.preferences/experimental.js
@@ -50,7 +50,7 @@ define(function(require, exports, module) {
             drawn = true;
             
             intro.$int.innerHTML = 
-                '<h1>Experimental Features</h1><p>Cloud9 is continuously in '
+                '<h1>Experimental Features</h1><p style="white-space:normal">Cloud9 is continuously in '
                 + 'development. New features in alpha or beta are first hidden '
                 + 'and can be enabled via this page. <i>Use at your own risk</i></p>';
         }
