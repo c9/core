cf02d0a1b8989a733521268f879f733da79688a4
Fabian Jakobs <fabian@c9.io>
2015-10-13 11:55:31 +0000

    add user-content to server.js

--- server.js
+++ server.js
@@ -23,14 +23,12 @@ var DEFAULT_CONFIG = "s";
 var DEFAULT_SETTINGS = getDefaultSettings();
 
 var shortcuts = {
-    "dev"  : ["ide", "preview", "vfs", "api", "sapi", "proxy", "redis", "profile", "oldclient", "homepage", "apps-proxy", "-s", "devel"],
-    "odev" : ["ide", "preview", "vfs", "api", "proxy", "oldclient", "homepage", "apps-proxy", "profile", "worker", "-s", "onlinedev"],
-    "bill" : ["ide", "preview", "vfs", "api", "proxy", "oldclient", "homepage", "apps-proxy", "profile", "-s", "billing"],
-    "beta" : ["ide", "preview", "vfs", "proxy", "-s", "beta"],
-    "ci"   : ["ide", "preview", "vfs", "proxy", "-s", "ci"],
+    "dev"  : ["ide", "preview", "user-content", "vfs", "api", "sapi", "proxy", "redis", "profile", "oldclient", "homepage", "apps-proxy", "-s", "devel"],
+    "odev" : ["ide", "preview", "user-content", "vfs", "api", "proxy", "oldclient", "homepage", "apps-proxy", "profile", "worker", "-s", "onlinedev"],
+    "beta" : ["ide", "preview", "user-content", "vfs", "proxy", "-s", "beta"],
     "s"    : ["standalone", "-s", "standalone"]
 };
-var delayLoadConfigs = ["preview", "api", "oldclient", "apps-proxy", "worker"];
+var delayLoadConfigs = ["preview", "user-content", "api", "oldclient", "apps-proxy", "worker"];
 
 module.exports = main;
 
