b361151d66f983c6666dc48d31ee81f0a945012b
Ruben Daniels <ruben@c9.io>
2015-10-21 14:59:29 -0700

    Revent changing hasValue to before setValue

--- plugins/c9.ide.editors/document.js
+++ plugins/c9.ide.editors/document.js
@@ -386,8 +386,8 @@ define(function(require, module, exports) {
                 },
                 set value(v) { 
                     value = recentValue = v;
-                    hasValue = true;
                     emit("setValue", { value: v });
+                    hasValue = true;
                 },
                 /**
                  * The last serialized value that was either set or retrieved.
