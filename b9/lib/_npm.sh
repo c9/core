0f9cbe73d116aa3b077f246d4a0a4c9bb5cc0951
Fabian Jakobs <fabian@c9.io>
2016-02-05 15:39:14 +0000

    mount all of ~/.ssh into the container

--- b9/lib/_npm.sh
+++ b9/lib/_npm.sh
@@ -1,7 +1,7 @@
 _b9_npm() {
     local WORKDIR=$1
     shift
-    docker run --rm -w /home/ubuntu/newclient -v $WORKDIR:/home/ubuntu/newclient -v $HOME/.ssh/id_rsa_deploy:/home/ubuntu/.ssh/id_rsa_deploy:ro --sig-proxy -a STDIN -a STDOUT -a STDERR $(_b9_get_newclient_image) npm "$@"
+    docker run --rm -w /home/ubuntu/newclient -v $WORKDIR:/home/ubuntu/newclient -v $HOME/.ssh:/home/ubuntu/.ssh:ro --sig-proxy -a STDIN -a STDOUT -a STDERR $(_b9_get_newclient_image) npm "$@"
     # pushd $WORKDIR
     # npm "$@"
     # popd
