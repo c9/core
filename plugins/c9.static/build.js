95de252cefc77486c569c8aa33615dfd06eb12ab
Lennart Kats <lclkats@gmail.com>
2016-02-24 13:37:47 +0000

    Remove obsolete mode_completer
    codeintel completer replaces it

--- plugins/c9.static/build.js
+++ plugins/c9.static/build.js
@@ -252,7 +252,6 @@ function main(options, imports, register) {
                 "plugins/c9.ide.language/worker",
                 "plugins/c9.ide.language.generic/local_completer",
                 "plugins/c9.ide.language.generic/snippet_completer",
-                "plugins/c9.ide.language.generic/mode_completer",
                 "plugins/c9.ide.language.generic/open_files_local_completer",
                 "plugins/c9.ide.language.generic/simple/make",
                 "plugins/c9.ide.language.generic/simple/shell",
