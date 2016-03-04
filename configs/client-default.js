7f0e3773bf7596847c96cc2f46f3de4e7ed31776
Brady Dowling <bradydowling@gmail.com>
2016-03-04 14:09:37 +0000

    Included starter guide

--- configs/client-default.js
+++ configs/client-default.js
@@ -731,6 +731,10 @@ module.exports = function(options) {
             staticPrefix: staticPrefix + "/plugins/c9.ide.help"
         },
         {
+            packagePath: "plugins/c9.ide.guide/guide",
+            staticPrefix: staticPrefix + "/plugins/c9.ide.guide"
+        },
+        {
             packagePath: "plugins/c9.ide.configuration/configure",
             dashboardUrl: options.dashboardUrl,
         },
