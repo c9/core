ab3913a429c4fed65c135ce976415cc88b5383c9
Fabian Jakobs <fabian@c9.io>
2016-04-12 10:26:51 +0000

    decodeURIComponent can throw
    fixes https://github.com/c9/newclient/issues/13386

--- plugins/c9.preview/lib/middleware/sanitize-path-param.js
+++ plugins/c9.preview/lib/middleware/sanitize-path-param.js
@@ -1,9 +1,16 @@
 "use strict";
 
 var Path = require("path");
+var error = require("http-error");
 
 module.exports = function sanitzePreviewPath(req, res, next) {
-    var normalized = Path.normalize(decodeURIComponent(req.params.path));
+    
+    var normalized;
+    try {
+        normalized = Path.normalize(decodeURIComponent(req.params.path));
+    } catch(e) {
+        return next(new error.BadRequest("URI malformed"));
+    }
 
     // N.B. Path.normalize does not strip away when the path starts with "../"
     if (normalized)
