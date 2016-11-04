53fe708d8ca8dbb36ebcd92757d5ac1b7a3cc8a5
Fabian Jakobs <fabian@c9.io>
2016-11-04 07:56:52 +0000

    unblacklist metadata test. wasn't broken in v3

--- plugins/c9.ide.editors/metadata_test.js
+++ plugins/c9.ide.editors/metadata_test.js
@@ -107,7 +107,7 @@ require(["lib/architect/architect", "lib/chai/chai", "/vfs-root"],
                 return tab.pane.aml.getPage("editor::" + tab.editorType).$ext;
         });
         
-        describe.skip('metadata', function() {
+        describe('metadata', function() {
             this.timeout(20000);
             
             before(function(done) {
