5f778b86c93478f780e487f4d86a2949821e5948
Jaime Alvarez <jaime_andres@unitec.edu>
2017-05-31 01:54:53 +0000

    Allow Markdown outlining to show levels beyond level 2.
    Currently the outline for Markdown files shown in the outline panel
    only outlines level 1 and level 2 headings. This commit adds the
    remaining headings (3 to 6).

--- plugins/c9.ide.language.jsonalyzer/worker/handlers/jsonalyzer_md.js
+++ plugins/c9.ide.language.jsonalyzer/worker/handlers/jsonalyzer_md.js
@@ -13,10 +13,16 @@ var ctagsUtil = require("plugins/c9.ide.language.jsonalyzer/worker/ctags/ctags_u
 var TAGS = [
     { regex: /(?:^|\n)# (.*?)[#\s]*(?:\n|$)/g, kind: "property" },
     { regex: /(?:^|\n)([A-Za-z0-9].*?)\s*\n={2,}(?:\n|$)/g, kind: "property" },
-    { regex: /(?:^|\n)#{3,} (.*?)[#\s]*(?:\n|$)/g, kind: "property2", indent: 2 },
-    { regex: /(?:^|\n)([A-Za-z0-9].*?)\s*\n-{3,}(?:\n|$)/g, kind: "property2", indent: 2 },
     { regex: /(?:^|\n)#{2} (.*?)[#\s]*(?:\n|$)/g, kind: "property2", indent: 1 },
     { regex: /(?:^|\n)([A-Za-z0-9].*?)\s*\n-{2}(?:\n|$)/g, kind: "property2", indent: 1 },
+    { regex: /(?:^|\n)#{3} (.*?)[#\s]*(?:\n|$)/g, kind: "property2", indent: 2 },
+    { regex: /(?:^|\n)([A-Za-z0-9].*?)\s*\n-{3,}(?:\n|$)/g, kind: "property2", indent: 2 },
+    { regex: /(?:^|\n)#{4} (.*?)[#\s]*(?:\n|$)/g, kind: "property2", indent: 3 },
+    { regex: /(?:^|\n)([A-Za-z0-9].*?)\s*\n-{4,}(?:\n|$)/g, kind: "property2", indent: 3 },
+    { regex: /(?:^|\n)#{5} (.*?)[#\s]*(?:\n|$)/g, kind: "property2", indent: 4 },
+    { regex: /(?:^|\n)([A-Za-z0-9].*?)\s*\n-{5,}(?:\n|$)/g, kind: "property2", indent: 4 },
+    { regex: /(?:^|\n)#{6} (.*?)[#\s]*(?:\n|$)/g, kind: "property2", indent: 5 },
+    { regex: /(?:^|\n)([A-Za-z0-9].*?)\s*\n-{6,}(?:\n|$)/g, kind: "property2", indent: 5 },
 ];
 var GUESS_FARGS = false;
 var EXTRACT_DOCS = false;
@@ -30,10 +36,10 @@ handler.extensions = ["md", "markdown"];
 handler.analyzeCurrent = function(path, doc, ast, options, callback) {
     if (doc === "")
         return callback(null, {});
-        
+
     if (doc.length > jsonalyzer.getMaxFileSizeSupported())
         return callback(null, {});
-    
+
     var results = {};
     TAGS.forEach(function(tag) {
         if (tag.kind === "import")
