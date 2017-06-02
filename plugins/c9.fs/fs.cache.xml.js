661166ea43801f89393fca059d3a2a8ebd43395d
nightwing <amirjanyan@gmail.com>
2017-06-02 12:31:21 +0400

    fix salesforce newfile plugin

--- plugins/c9.fs/fs.cache.xml.js
+++ plugins/c9.fs/fs.cache.xml.js
@@ -138,8 +138,12 @@ define(function(require, exports, module) {
                         toRemove.push(name);
                 });
                 
-                if (!toCreate.length && !toRemove.length && !orphanAppand.length)
-                    return;
+                if (!toCreate.length && !toRemove.length && !orphanAppand.length) {
+                    // emit readdir event for empty folders, since other plugins may use that
+                    // to display create new file buttons
+                    if (node.children && node.children.length)
+                        return;
+                }
                 
                 var wasOpen = startUpdate(node);
                 node.children = null;
